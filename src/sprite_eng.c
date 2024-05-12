3a70d2c50ed0aec57a04d4e7e613077f786f67b0
hiroshica <hiroshica@gmail.com>
2024-05-12 11:54:40 +0900

    反転とかを計算でしやすいようにSPRITE中央を原点として計算できるようにした

--- src/sprite_eng.c
+++ src/sprite_eng.c
@@ -1601,7 +1601,10 @@ void NO_INLINE SPR_update()
                 u16 attr = sprite->attribut&TILE_FLIP_MASK;
                 u16 tileoffset = sprite->attribut&0x7ff;
                 u16 ppattr = sprite->attribut&0xe000;
+                u16 xsize = (((sprite->attribut>>2)&0x0003)+1)<<3;
+                u16 ysize = (((sprite->attribut>>0)&0x0003)+1)<<3;
                 u8 nums = frame->numSprite;
+
                 while(nums)
                 {
                     u16 hvattr = attr^(frameSprite->attribute&TILE_FLIP_MASK);
@@ -1612,8 +1615,8 @@ void NO_INLINE SPR_update()
                         case TILE_FLIP_MASK:
                         case 0:
                             {
-                                vdpSprite->y = sprite->y + frameSprite->offsetY;
-                                vdpSprite->x = sprite->x + frameSprite->offsetX;
+                                vdpSprite->y = (sprite->y + frameSprite->offsetY) + (ysize>>1);
+                                vdpSprite->x = (sprite->x + frameSprite->offsetX) + (xsize>>1);
                                 vdpSprite->size = frameSprite->attribute&0xff;
                                 vdpSprite->link = vdpSpriteInd++;
                                 vdpSprite->attribut = (tileoffset + frameSprite->TileOffset) | hvattr | ppattr;
